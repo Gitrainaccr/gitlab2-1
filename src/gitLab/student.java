b36deacf661a290146d229d2c3f28a5387f2c19b
leonardtig <78554470+leonardtig@users.noreply.github.com>
2021-02-17 09:32:53 +0530

    Update student.java

--- src/gitLab/student.java
+++ src/gitLab/student.java
@@ -4,13 +4,7 @@ public class student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x;
-		int y;
-		int z; 
-		System.out.println("hello world");
-
-		int a;
-		int b;
+		
 
 	}
 
